8cc98eab00829ae22fa8318c43bb7572d350f303
Dmitry Bulavko <you@example.com>
2023-10-29 21:08:00 +0700

    task1Fix

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,13 @@
 public class Task3 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int temp = arr[0];
+        arr[0] = arr[arr.length-1];
+        arr[arr.length-1] = temp;
+
+        System.out.print(arr[0]+arr[arr.length-3]);
+    }
 }
