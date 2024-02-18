b4eb5c83644e686b9411514a9f2998060ac377ff
Dmitry Bulavko <you@example.com>
2024-02-18 13:07:42 +0700

    task1

--- src/Task1_1.java
+++ src/Task1_1.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Task1_1 {
+import java.util.Scanner;
+
+public class Task1_1 {
+
+    /**
+     * @param args
+     * @author Dmitrii Bulavko
+     * @see #calc()
+     */
+    public static void main(String[] args) {
+
+        System.out.printf("Результат вычесления %.4f", calc());
+
+    }
+
+    /**
+     * @return double result
+     */
+    public static double calc() {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = Double.parseDouble(scanner.nextLine());
+        double y = Double.parseDouble(scanner.nextLine());
+        String z = scanner.nextLine();
+        double result = 0;
+
+        switch (z) {
+            case ("+"):
+                result = x + y;
+                break;
+            case ("-"):
+                result = x - y;
+                break;
+            case ("*"):
+                result = x * y;
+                break;
+            case ("/"):
+                result = x / y;
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
+
 }
